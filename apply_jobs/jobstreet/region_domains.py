eead2b64a5bccb555c4c2b4699f37252a85b0eca
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-04-17 20:07:47 +0700

    region_domains for jobstreets package

---
+++ apply_jobs/jobstreet/region_domains.py
@@ -0,0 +1,7 @@
+region_domains = {
+    "malaysia": "https://www.jobstreet.com.my/?icmpid=js_global_landing_page",
+    "singapore": "https://www.jobstreet.com.sg/?icmpid=js_global_landing_page",
+    "philipines": "https://www.jobstreet.com.ph/?icmpid=js_global_landing_page",
+    "indonesia": "https://www.jobstreet.co.id/?icmpid=js_global_landing_page",
+    "vietnam": "https://www.jobstreet.vn/?icmpid=js_global_landing_page",
+}
